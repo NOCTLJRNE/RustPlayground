a87837e848522224dbbc4beb620a1077db0f3c73
NOCTLJRNE <baoan1602@gmail.com>
2020-08-01 16:55:59 +0200

    add generic

---
+++ generic/src/main.rs
@@ -0,0 +1,19 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+fn main() {
+    let p1 = Point { x: 5, y: 3.14 };
+    let p2 = Point { x: "Hi", y: 'Y' };
+    let p3 = p1.mixup(p2);
+    // let p4 = p2.mixup(p1);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
